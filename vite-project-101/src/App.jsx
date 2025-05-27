ec01a9a4335267ab0c51ed4551a75c94d12583c3
Shakhoyat <shakoyatsujon@gmail.com>
2025-05-28 00:44:34 +0600

    Refactor App component to use Test component and clean up code structure

--- vite-project-101/src/App.jsx
+++ vite-project-101/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Person from './Person'
+import { Test } from './components/Test'
+// Importing the Person component and Test component from the components directory
 
-
-const App = () => {
+function App() {
   return (
     <>
       <h1>Welcome to the Vite Project</h1>
       <p>This is a simple React application using Vite.</p>
       <p>Feel free to explore and modify the code!</p>
       <p>Enjoy coding with Vite!</p>
-      <p>Happy coding!</p> <Person />
+      <p>Happy coding!</p>
+      {/* <Person /> */}
+      <Test />
     </>
   )
 }
