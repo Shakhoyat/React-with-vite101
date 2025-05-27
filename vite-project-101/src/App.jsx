044b6377dc42695ba093cf941d0ff0a14ba5ae26
Shakhoyat <shakoyatsujon@gmail.com>
2025-05-27 23:06:19 +0600

    Refactor App component to use variables for user data and improve readability

--- vite-project-101/src/App.jsx
+++ vite-project-101/src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
 const App = () => {
+  const name= 'Skt'
+  const age = 25
+  const isLoggedIn = true
+  const skills = ['JavaScript', 'React', 'Node.js']
+  const user = {
+    name: 'Skt',
+    age: 25,
+    isLoggedIn: true,
+    skills: ['JavaScript', 'React', 'Node.js']
+  }
   return (
     <div>
       <h1>MERN Development</h1>
-      <h2>Skt</h2>
-      <h3>Im learning React.js</h3>
+      <h2>{name}</h2>
+      <p>age = {age}</p>
+      <h3>Im learning React.js...My other skills are:</h3>
+      <h3>{skills}</h3>
       <h1>2+5</h1>
       <h1>{2+5}</h1>
     </div>
