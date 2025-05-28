e49ec66aedeaa9fe5bb0f83182fc0991d649e298
Shakhoyat <shakoyatsujon@gmail.com>
2025-05-28 12:56:25 +0600

    event handling with passing some extra argument

--- vite-project-101/src/components/Events.jsx
+++ vite-project-101/src/components/Events.jsx
@@ -4,10 +4,15 @@ const Events = () => {
     const handleClick = () => {
         alert('Button clicked!');
     }
+    const additionalFunction = (a) => {
+        alert('Additional function executed with argument: ' + a);
+    }
     return (
         <div>
             <div>We are learning Events</div>
             <button onMouseOver={handleClick}>Click the btn</button>
+            <button onClick={() => additionalFunction('Hello')}>Button2</button>
+            {/* ()=> is the callback function...means it will execute only when the button2 is clicked otherwise it will always get executed without any click */}
         </div>
 
     )
