cf534b67cb346113ae0b3c693ea8e8108dcc397d
Shakhoyat <shakoyatsujon@gmail.com>
2025-05-28 12:10:31 +0600

    external css

--- vite-project-101/src/components/Laptop.jsx
+++ vite-project-101/src/components/Laptop.jsx
@@ -1,19 +1,8 @@
 import React from 'react'
-
+import './laptop.css'; // Assuming you have a CSS file for styling
 const Laptop = ({ brandname, model, price }) => {
-    const obj = {
-        backgroundColor: 'lightblue',
-        padding: '20px',
-        borderRadius: '10px',
-        margin: '10px',
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-        transition: 'transform 0.2s',
-        textAlign: 'center'
-    }
     return (
-        // <div style={{ backgroundColor: 'lightblue', padding: '20px', borderRadius: '10px', margin: '10px' }}>
-        // <h3 style={{ fontWeight: 'bold', fontSize: '2rem', color: '#1a237e', textShadow: '1px 1px 4px #fff' }}>
-        <div style={obj}>
+        <div className='divv'>
             <h3>
                 Brand: {brandname} <br />
                 Model: {model} <br />
@@ -22,5 +11,4 @@ const Laptop = ({ brandname, model, price }) => {
         </div>
     )
 }
-
 export default Laptop
