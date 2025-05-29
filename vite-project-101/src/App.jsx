0473eae4f97d68ce1e41979d1bf4166daf4de1ae
Shakhoyat <shakoyatsujon@gmail.com>
2025-05-29 10:44:25 +0600

    Refactor App component to implement counter functionality with increment and decrement buttons

--- vite-project-101/src/App.jsx
+++ vite-project-101/src/App.jsx
@@ -1,25 +1,19 @@
 import React from 'react'
-import Person from './Person'
-import Shujon from './components/Test'
-// Importing the Person component and Test component from the components directory
-import Sohan from './components/Product'
-import Product from './components/Product'
-import Person2 from './components/Person2'
-import Person3 from './components/Person3'
-import Laptop from './components/Laptop'
-import Events from './components/Events'
-import Counter from './components/Counter'
-import ShowProducts from './components/ShowProducts'
-import FilterProduct from './components/FilterProduct'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const App = () => {
+  const [cnt, setcnt] = useState(0)
   useEffect(() => {
     console.log("Use Effect calling...");
   }, []);
   return (
     <>
-      <h1>Learning UseEffect</h1>
+      <h1>
+        {cnt}
+      </h1>
+      <button onClick={() => setcnt(cnt + 1)}>Increment</button>
+      <button onClick={() => setcnt(cnt - 1)}>Decrement</button>
+
     </>
   )
 }
