356c5ca6a0834618a7e0cec659d28b47cdb2762d
Shakhoyat <shakoyatsujon@gmail.com>
2025-05-29 13:23:46 +0600

    enhance form handling with controlled components

--- vite-project-101/src/components/Forms.jsx
+++ vite-project-101/src/components/Forms.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
-
+import { useState } from 'react'
 const Forms = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`Name: ${name}, Email: ${email}, Password: ${password} has been submitted successfully!`);
+        setName(''); // Reset the name field
+        setEmail(''); // Reset the email field
+        setPassword(''); // Reset the password field
+    }
     return (
         <div>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <div>
-                    Name : <input type="text" placeholder='Enter your name' />
+                    Name : <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder='Enter your name' />
                 </div>
                 <div>
-                    Email : <input type="email" placeholder='Enter your email' />
+                    Email : <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Enter your email' />
                 </div>
                 <div>
-                    Password : <input type="password" placeholder='Enter your password' />
+                    Password : <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Enter your password' />
                 </div>
                 <div>
                     <button style={{ padding: "10px 20px", backgroundColor: "#4CAF50", color: "white", border: "none", borderRadius: "5px" }}>Submit</button>
