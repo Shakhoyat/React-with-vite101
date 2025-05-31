61f975bcfec2af38793deb7b1692d9d2cbe9476b
Shakhoyat <shakoyatsujon@gmail.com>
2025-05-31 16:47:15 +0600

    organize routing in App component and update Home page structure

--- vite-project-101/src/App.jsx
+++ vite-project-101/src/App.jsx
@@ -1,15 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './pages/Home'
+import Home from './pages/Home';
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Products from "./pages/Products";
+import Team from "./pages/Team";
 
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/team" element={<Team />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </Router>
-
     </>
   )
 }
--- vite-project-101/src/pages/Home.jsx
+++ vite-project-101/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
     return (
-        <div>Home ala page</div>
+        <>Home ala page</>
     )
 }
 
