4298d22f3b0128cf31960ccb1ac645088c970977
Shakhoyat <shakoyatsujon@gmail.com>
2025-05-31 17:05:02 +0600

    update Products component to display a list of available products with details

--- vite-project-101/src/pages/Products.jsx
+++ vite-project-101/src/pages/Products.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
 const Products = () => {
+    const arr = [
+        { id: mern101, name: "MERN stack", price: 15000, duration: '5 months' },
+        { id: py102, name: "Python Full Stack", price: 12000, duration: '4 months' },
+        { id: java103, name: "Java Full Stack", price: 18000, duration: '6 months' },
+        { id: ds104, name: "Data Science", price: 20000, duration: '7 months' }
+    ];
     return (
-        <div>Products</div>
+        <div>
+            <h1>Products Page</h1>
+            <ul>
+                {arr.map((item) => (
+                    <li key={item.id}>
+                        {item.name} - ${item.price} - {item.duration}
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
