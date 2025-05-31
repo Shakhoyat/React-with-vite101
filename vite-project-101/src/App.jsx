414a91104109f4381222d9f09f0c5f3fb280975e
Shakhoyat <shakoyatsujon@gmail.com>
2025-05-31 17:26:02 +0600

    refactor: remove unused components and simplify App structure

--- vite-project-101/src/App.jsx
+++ vite-project-101/src/App.jsx
@@ -1,24 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Courses from "./pages/Courses";
-import Team from "./pages/Team";
+import React from 'react'
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </>
+    <div>App</div>
   )
 }
 
--- vite-project-101/src/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-const Home = () => {
-    return (
-        <div>Home ala page</div>
-    )
-}
-
-export default Home
