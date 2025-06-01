1b0a3e0793bcca5710e0a4e4911ee67d3de66921
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-01 12:08:07 +0600

    useNavigate Hook's usage

--- vite-project-101/src/pages/About.jsx
+++ vite-project-101/src/pages/About.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom';
 const About = () => {
+    const navigate = useNavigate();
+    const goToHome = () => {
+        navigate("/");
+    }
+    const goBack = () => {
+        navigate(-1);
+    }
     return (
-        <div>About</div>
+        <div>
+            <button onClick={goToHome}>Go to Home</button>
+            <button onClick={goBack}>Go Back</button>
+        </div>
     )
 }
 
