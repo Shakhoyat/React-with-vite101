fcb69273345d33dc4863245d7e735672042aed17
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-02 16:13:57 +0600

    dynamic navigation bar

--- vite-project-101/src/App.jsx
+++ vite-project-101/src/App.jsx
@@ -7,6 +7,8 @@ import About from './pages/About';
 import Course_Details from './pages/Course_Details';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav_bar from './components/Nav_bar';
+import Dashbord from './pages/Dashbord';
+import Profile from './pages/Profile';
 
 
 const App = () => {
@@ -21,6 +23,8 @@ const App = () => {
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
           <Route path="/courses/:id" element={<Course_Details />} />
+          <Route path="/dashboard" element={<Dashbord />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </Router>
       <h1>Welcome to the App</h1>
--- vite-project-101/src/components/Nav_bar.jsx
+++ vite-project-101/src/components/Nav_bar.jsx
@@ -1,15 +1,43 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 import './nav_bar.css'
 const Nav_bar = () => {
+    const [isLoggedin, setisLoggedin] = useState(false);
+    const navigate = useNavigate();
+    const handleLogin = () => {
+        setisLoggedin(true);
+        navigate('/dashboard');
+    }
+    const handleLogout = () => {
+        setisLoggedin(false);
+        navigate('/');
+    }
+
     return (
         <div className='nav_bar'>
             <Link to={"/"} className="left">React101Me</Link>
             <div className="right">
-                <Link className="items" to={"/courses"}>Courses</Link>
-                <Link className="items" to={"/team"}>Team</Link>
-                <Link className="items" to={"/contact"}>Contact</Link>
-                <Link className="items" to={"/about"}>About</Link>
+                {
+                    isLoggedin && (
+                        <>
+                            <Link className="items" to={"/courses"}>Courses</Link>
+                            <Link className="items" to={"/team"}>Team</Link>
+                            <Link className="items" to={"/dashboard"}>Dashboard</Link>
+                            <Link className="items" to={"/profile"}>Profile</Link>
+                            <button className='items' onClick={handleLogout} style={{ fontWeight: 'bold', backgroundColor: "-moz-initial" }}>LogOut</button>
+                        </>
+                    )
+                }
+                {
+                    !isLoggedin && (
+                        <>
+                            <Link className="items" to={"/about"}>About</Link>
+                            <Link className="items" to={"/contact"}>Contact</Link>
+                            <button className='items' onClick={handleLogin} style={{ fontWeight: 'bold', backgroundColor: "-moz-initial" }}>Login</button>
+                        </>
+                    )
+                }
             </div>
         </div>
     )
