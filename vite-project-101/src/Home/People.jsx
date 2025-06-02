bbbaaa2b30adedad80b9106f73d97db8518de3c8
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-02 17:51:06 +0600

    refactor: update People component to use context data directly

--- vite-project-101/src/Home/People.jsx
+++ vite-project-101/src/Home/People.jsx
@@ -2,11 +2,14 @@ import React, { useContext } from 'react'
 import MoneyContext from '../context/MoneyContext'
 
 const People = () => {
-    const { money } = useContext(MoneyContext); // <-- extract money from context
-    console.log(useContext(MoneyContext));
+    // const { money } = useContext(MoneyContext); // <-- extract money from context
+    // console.log(useContext(MoneyContext));
+    const data = useContext(MoneyContext);
     return (
         <>
-            <h3>This is People Component and we have {money} money now.</h3>
+            <h3>This is People Component and we have {data.money} Tk ... </h3>
+            <h3>Dollar: {data.dollar} </h3>
+            <h3>Euro: {data.euro} </h3>
         </>
     )
 }
